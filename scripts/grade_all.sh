1099c1ed1aa62f67acfb23c1840d25dff9bc799b
Vu Van Huy <huyvv10@fpt.edu.vn>
2026-05-03 13:21:26 +0700

    Update grade_all.sh

--- scripts/grade_all.sh
+++ scripts/grade_all.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 total_score=0
-total_questions=0
+total_tests=0
 
 for q in Q*
 do
-  echo "===== Grading $q ====="
+  echo "===== $q ====="
 
   if [ ! -f "$q/main.c" ]; then
-    echo "❌ Missing main.c"
+    echo "Missing main.c"
     continue
   fi
 
   gcc "$q/main.c" -o "$q/main"
 
   if [ $? -ne 0 ]; then
-    echo "❌ Compile Error"
+    echo "Compile Error"
     continue
   fi
 
@@ -32,15 +32,18 @@ do
     diff -q output.txt $expected > /dev/null
 
     if [ $? -eq 0 ]; then
+      echo "Test $id: PASS"
       score=$((score+1))
+    else
+      echo "Test $id: FAIL"
     fi
   done
 
   echo "Score $q: $score/$total"
 
   total_score=$((total_score + score))
-  total_questions=$((total_questions + total))
+  total_tests=$((total_tests + total))
 done
 
 echo "======================="
-echo "FINAL SCORE: $total_score/$total_questions"
+echo "FINAL SCORE: $total_score/$total_tests"
