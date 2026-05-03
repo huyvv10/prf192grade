c9bfe1ee4a7618b9c88d20cfdb62c164ecc8389b
Vu Van Huy <huyvv10@fpt.edu.vn>
2026-05-03 09:29:34 +0700

    Create grade_all.sh

---
+++ scripts/grade_all.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+total_score=0
+total_questions=0
+
+for q in Q*
+do
+  echo "===== Grading $q ====="
+
+  if [ ! -f "$q/main.c" ]; then
+    echo "❌ Missing main.c"
+    continue
+  fi
+
+  gcc "$q/main.c" -o "$q/main"
+
+  if [ $? -ne 0 ]; then
+    echo "❌ Compile Error"
+    continue
+  fi
+
+  score=0
+  total=$(ls $q/testcases/input*.txt 2>/dev/null | wc -l)
+
+  for input in $q/testcases/input*.txt
+  do
+    id=$(basename $input | grep -o '[0-9]*')
+    expected="$q/testcases/output$id.txt"
+
+    "$q/main" < $input > output.txt
+
+    diff -q output.txt $expected > /dev/null
+
+    if [ $? -eq 0 ]; then
+      score=$((score+1))
+    fi
+  done
+
+  echo "Score $q: $score/$total"
+
+  total_score=$((total_score + score))
+  total_questions=$((total_questions + total))
+done
+
+echo "======================="
+echo "FINAL SCORE: $total_score/$total_questions"
